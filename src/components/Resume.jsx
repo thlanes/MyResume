8324471e03fd2c85003c91f8ec5972886bdc2fb4
Thiago Lanes <thiagolanes@Thiagos-MacBook-Pro.local>
2023-11-30 12:00:27 -0700

    add link on Header.jsx and simplied the Resume.jsx with arrow function.

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,21 +2,19 @@ import { React } from "react";
 import Card from "./Card";
 import resumeDetails from "../resumeDetails";
 
-function createExperience(resumeDetails) {
-    return <Card
-        key={resumeDetails.id} 
-        position={resumeDetails.position}
-        company={resumeDetails.company}
-        details={resumeDetails.details}
-    />;
-}
 
 function Resume() {
     return (
         <div className="container">
             <h1>Professional Experience</h1>
             <br />
-            {resumeDetails.map(createExperience)}
+            {resumeDetails.map(resumeDetails => (<Card
+        key={resumeDetails.id} 
+        position={resumeDetails.position}
+        company={resumeDetails.company}
+        details={resumeDetails.details}
+    />))};
+
         </div>
 
     );
