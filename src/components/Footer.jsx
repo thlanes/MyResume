bc20936cd8a8466860a59c45562c1e50a51b215a
Thiago Lanes <thiagolanes@Thiagos-MacBook-Pro.local>
2023-12-25 22:41:18 -0700

    Included mu LinkedIn link on the foother. Clickable image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import { React } from "react";
+import linkedin from "../pictures/linkedin-rounded-svgrepo-com.svg";
 
 function Footer() {
     return (
         <div className="container gradient-background">
             <footer class="py-3 my-4">
             <p className="text-center text-body-secondary">© {new Date().getFullYear()} Thiago Lanes</p>
+            <a href="https://www.linkedin.com/in/thiagovlanes/" rel="noreferrer noopener" target="_blank"><img src={linkedin} alt="js" height="30" /></a>
             </footer>
         </div>
     )
